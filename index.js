dfdfa3853a0d44e526734a51109d1d515cc005e2
arthsindekar <sindekar.a@northeastern.edu>
2025-11-17 14:15:55 -0500

    allowed headers authorization

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
   cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     origin: process.env.CLIENT_URL || "http://localhost:3000",
   })
 );
